504b15dd89ff3c41f91873bbc77327a40b996c27
Zhou Sun <ZHOUSUN@USC.EDU>
2019-07-24 15:37:48 +0800

    no bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :show, :create] do
+    resources :reviews, only: [:create, :new]
+  end
+
+  root to: 'restaurants#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all if Rails.env.development?
 
 5.times do
-  Restaurant.create!({name: Faker::Restaurant.name , address: Faker::Address.street_address, category: ['chinese','italian','japanese','french','belgian'].sample, phone_number: Faker::IDNumber.south_african_id_number})
+  Restaurant.create!({name: Faker::Restaurant.name, address: Faker::Address.street_address, category: ['chinese','italian','japanese','french','belgian'].sample, phone_number: Faker::IDNumber.south_african_id_number})
 end
+
+20.times do
+  Review.create!({content: Faker::Quote.famous_last_words, rating: [0,1,2,3,4,5].sample, restaurant_id: [13,14,15,16,17].sample})
+end
+
 p "created"
